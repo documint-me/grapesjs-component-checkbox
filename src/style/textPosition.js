9039fa700b2d5ffb19f77d9d1ad0402ad24d9263
Brendon Ngirazi <48953676+Ju99ernaut@users.noreply.github.com>
2023-06-11 16:22:48 +0200

    Text position functionality

--- src/style/textPosition.js
+++ src/style/textPosition.js
@@ -10,7 +10,7 @@ export default function (editor, opts = {}) {
     ...opts,
   };
 
-  const { name, defaultValues } = opts;
+  const { name, defaultValues, labelLeft, labelRight } = opts;
   const sm = editor.StyleManager;
 
   sm.addType(name, {
@@ -21,27 +21,65 @@ export default function (editor, opts = {}) {
       el.innerHTML = `<div class="gjs-field gjs-field-radio">
         <div class="gjs-radio-items">
           <div class="gjs-radio-item">
-            <input type="radio" class="gjs-sm-radio gjs-sm-radio--" id="--left-c533" name="--c533" value="">
-            <label class="gjs-radio-item-label" for="--left-c533">Left</label>
+            <input type="radio" class="gjs-sm-radio" id="checkbox-text-left" name="pos" value="left" ${
+              defaultValues.position === "left" ? "checked" : ""
+            }>
+            <label class="gjs-radio-item-label" for="checkbox-text-left">${labelLeft}</label>
           </div>
           <div class="gjs-radio-item">
-            <input type="radio" class="gjs-sm-radio gjs-sm-radio--" id="--right-c533" name="--c533" value="right" checked="">
-            <label class="gjs-radio-item-label" for="--right-c533">Right</label>
+            <input type="radio" class="gjs-sm-radio" id="checkbox-text-right" name="pos" value="right" ${
+              defaultValues.position === "right" ? "checked" : ""
+            }>
+            <label class="gjs-radio-item-label" for="checkbox-text-right">${labelRight}</label>
           </div>
         </div>
       </div>
       `;
+
+      this.inputEl = el;
+
+      const radios = el.querySelectorAll('[name="pos"]');
+      radios.forEach((radio) => {
+        radio.addEventListener("change", ({ target }) => {
+          this.setLabelPosition(target.value);
+        });
+      });
 
       this.em.on("component:selected", () => this.updateUI());
 
       return el;
     },
 
-    // Propagate UI changes to target
-    emit({ updateStyle }, { css, partial }) {},
-
     // Update UI when target is changed
-    updateUI() {},
+    updateUI() {
+      const { pos, label } = this.getLabelPosition();
+
+      // set UI value based on label flex order
+      const inputIn =
+        this.inputEl && this.inputEl.querySelector(`[value="${pos}"]`);
+      inputIn && (inputIn.checked = true);
+    },
+
+    setLabelPosition(inPos) {
+      const { pos, label } = this.getLabelPosition();
+
+      // set style based on value from UI
+      label.addStyle({ order: inPos === "right" ? "1" : "-1" });
+    },
+
+    getLabelPosition() {
+      const checkbox = this.em.getSelected();
+
+      if (!checkbox || !checkbox.getLabel) return {};
+
+      const label = checkbox.getLabel();
+
+      if (!label || !label.getStyle) return {};
+
+      const style = label.getStyle().order;
+
+      return { pos: style === "-1" ? "" : "right", label };
+    },
 
     // Clean memory if necessary
     destroy() {},
