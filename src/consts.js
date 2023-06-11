0f8ae900504ac9d5f622d19514d412e580e4b69d
Brendon Ngirazi <48953676+Ju99ernaut@users.noreply.github.com>
2023-06-11 16:23:00 +0200

    formating

--- src/consts.js
+++ src/consts.js
@@ -1,29 +1,47 @@
 export const TYPES = {
-  label: 'dm-label',
-  toggle: 'dm-toggle',
-  checkbox: 'dm-checkbox',
-}
+  label: "dm-label",
+  toggle: "dm-toggle",
+  checkbox: "dm-checkbox",
+};
 
 export const BLOCKS = {
-  checkbox: 'dm-checkbox',
-}
+  checkbox: "dm-checkbox",
+};
 
 export const ACTIONS = {
-  addComponent: 'add-component',
-  removeComponent: 'remove-component',
-  cloneComponent: 'clone-component',
-  moveComponent: 'move-component',
-  selected: 'selected',
-}
+  addComponent: "add-component",
+  removeComponent: "remove-component",
+  cloneComponent: "clone-component",
+  moveComponent: "move-component",
+  selected: "selected",
+};
 
-export const RESIZER_NONE = { tl: false, tc: false, tr: false, cr: false, br: false, bc: false, bl: false, cl: false }
-export const RESIZER_ALL = { tl: true, tc: true, tr: true, cr: true, br: true, bc: true, bl: true, cl: true }
+export const RESIZER_NONE = {
+  tl: false,
+  tc: false,
+  tr: false,
+  cr: false,
+  br: false,
+  bc: false,
+  bl: false,
+  cl: false,
+};
+export const RESIZER_ALL = {
+  tl: true,
+  tc: true,
+  tr: true,
+  cr: true,
+  br: true,
+  bc: true,
+  bl: true,
+  cl: true,
+};
 
-export default { TYPES, ACTIONS, RESIZER_NONE, RESIZER_ALL }
+export default { TYPES, ACTIONS, RESIZER_NONE, RESIZER_ALL };
 
 export const RESIZABLE_PROPS = {
-  startX: 'startX',
-  prevX: 'prevX',
-  prevDirection: 'prevDirection',
-  prevDeltaX: 'prevDeltaX',
-}
+  startX: "startX",
+  prevX: "prevX",
+  prevDirection: "prevDirection",
+  prevDeltaX: "prevDeltaX",
+};
